cd4de186be4b5e7f0776504f8a39035eea23472b
3ammor <o.3ammor@gmail.com>
2017-03-12 00:18:34 +0200

    I still have no idea what I'm doing 21

--- public/js/drawing.js
+++ public/js/drawing.js
@@ -93,7 +93,7 @@ window.onload = function () {
     var tool = new Tool();
 
 
-    socket.emit('join', myRoom);
+    socket.emit('join', myRoom, myId);
 
 
     tool.onMouseDown = function (event) {
