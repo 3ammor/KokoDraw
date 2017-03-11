a0c9af163c18655516dbcb391c27f25682fa511a
Mostafa Gamal <mgo_oc@hotmail.com>
2017-03-12 00:34:55 +0200

    The best till now 7.

--- models/room.js
+++ models/room.js
@@ -3,7 +3,7 @@ module.exports = function(sequelize, DataTypes) {
   var Room = sequelize.define('Room', {
     id: {type: DataTypes.INTEGER, primaryKey: false, unique: true, autoIncrement: true},
     name: {type: DataTypes.STRING, primaryKey: true},
-    data: DataTypes.STRING
+    data: DataTypes.TEXT('long')
   },	 {
     classMethods: {
       associate: function(models) {
